438d178937f11c67b4f0d9637554d44f1abb80a1
Bence <m.bence05@gmail.com>
2020-10-10 18:53:09 +0200

    authentifikacne komponenty

--- src/app/shared/directives/submit/submit.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SubmitDirective } from './submit.directive';
-
-describe('SubmitDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SubmitDirective();
-    expect(directive).toBeTruthy();
-  });
-});
