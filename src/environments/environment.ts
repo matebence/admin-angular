6cf71ba9dfa7771e54f631b77381dfe6fd5e228d
Bence <m.bence05@gmail.com>
2020-10-10 11:50:46 +0200

    zakladna struktura aplikacie

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,21 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
+  PRODUCTION: false,
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  HOST_IP_API: 'http://ip-api.com',
+  HOST_IPIFY: 'https://api.ipify.org',
+  HOST_BLESK: 'http://192.168.99.100:8765',
+  HOST_BLESK_WEBSOCKET: 'http://192.168.99.100:8765/websocket',
+
+  GRANT_TYPE_PASSWORD: 'password',
+  GRANT_TYPE_REFRESH_TOKEN: 'refresh_token',
+
+  APP_ROLE_ADMIN: 'ROLE_ADMIN',
+  APP_ROLE_CLIENT: 'ROLE_CLIENT',
+  APP_ROLE_MANAGER: 'ROLE_MANAGER',
+  APP_ROLE_COURIER: 'ROLE_COURIER',
+
+  CLIENT_ID: '8de4dc56-6e96-11ea-bc55-0242ac130003',
+  CLIENT_SECRET: '04ed73c86f2d48e9789fa2d2bae6acf78bcd9b431d676e5f6d9c9c9a28de29a3',
+
+  HERE_JS_SDK_API: 'cg148a7i1R6OXqhpPmkHUBvCr07I-qJKX_Cf2qpo_KU'
+};
