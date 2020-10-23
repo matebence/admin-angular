eb6c2be418109065c90473d21a786788f30ccec3
Bence <m.bence05@gmail.com>
2020-10-23 16:41:57 +0200

    route filter fix

--- src/app/core/filter/route.filter.ts
+++ src/app/core/filter/route.filter.ts
@@ -26,7 +26,8 @@ export class RouteFilter {
   }
 
   private autoSignIn(navigationEnd: NavigationEnd): void {
-    if (navigationEnd.url.includes('sign-out') || !navigationEnd.url.includes('dashboard')) {
+    const segments: string[] = navigationEnd.url.split('/');
+    if (!segments.includes('sign-out') && !segments.includes('dashboard')) {
       const signIn = <SignIn> this.persistenceService.get(environment.LOCAL_STORAGE_ACCOUNT_DATA);
       if (signIn == null || new Date() >= signIn.expirationDate) return;
       this.router.navigate(['/dashboard/home']);
