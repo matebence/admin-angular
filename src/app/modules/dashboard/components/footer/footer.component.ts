0530092d9647ef3f4042328deb0ed16c034e81c0
Bence <m.bence05@gmail.com>
2020-10-24 11:45:16 +0200

    prisposobenie temy

--- src/app/modules/dashboard/components/footer/footer.component.ts
+++ src/app/modules/dashboard/components/footer/footer.component.ts
@@ -7,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public year: number = new Date().getFullYear();
+
   public constructor() {
   }
 
