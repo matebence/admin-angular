934aca813e7b7517d59c36b93ce376ca7b52c643
Bence <m.bence05@gmail.com>
2020-11-07 18:52:07 +0100

    placeholder pre crud operacie

--- src/app/modules/dashboard/pages/user/components/payments/payments-form/payments-form.component.ts
+++ src/app/modules/dashboard/pages/user/components/payments/payments-form/payments-form.component.ts
@@ -1,16 +1,46 @@
-import {Component, OnInit} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+import {CanComponentDeactivate} from '../../../../../../../core/guards/leave.guard';
 
 @Component({
   selector: 'app-payments-form',
   templateUrl: './payments-form.component.html',
   styleUrls: ['./payments-form.component.css']
 })
-export class PaymentsFormComponent implements OnInit {
+export class PaymentsFormComponent implements OnInit, OnDestroy, CanComponentDeactivate {
+
+  public text: string;
+  public title: string;
+  public negativeButton: string;
+  public pozitiveButton: string;
+
+  public illustration: string = 'assets/img/place_asistent.svg';
+  public assistent: string = 'Výtajte som Váš osobný asistent. Som tu aby som pomohol a vysvetloval. Momentálne sa chystáte vytvoriť nový kraj pre aplikáciu Blesk.';
+  public assistentOptions: any = [{title: 'Okresy', link: '/dashboard/services/places/table/districts'}, {title: 'Mestá a obce', link: '/dashboard/services/places/table/villages'}];
+
+  public formButton: string = 'Vytvoriť';
+  public formTitle: string = 'Vytvorenie nového kraja';
+
+  private subscriptions: Subscription[] = [];
 
   public constructor() {
   }
 
   public ngOnInit(): void {
     return;
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.forEach(e => e.unsubscribe());
+    return;
+  }
+
+  public onSubmit(): void {
+    return;
+  }
+
+  public canDeactivate(event: boolean): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
   }
 }
