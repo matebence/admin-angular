5687bc0fc5ed639556412b8630dd562e348fe3cf
Bence <m.bence05@gmail.com>
2020-11-19 18:04:52 +0100

    Revert "path mapper pre proxy"
    This reverts commit a4990a322ff1c87569004c647910af80c8fe7cef.

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -6,50 +6,28 @@ import {ErrorPage} from './error/error.page';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/manage/auth/sign-in',
+    redirectTo: '/auth/sign-in',
     pathMatch: 'full'
   },
   {
-    path: 'manage',
-    redirectTo: '/manage/auth/sign-in',
+    path: 'auth/sign-in',
+    redirectTo: '/auth/sign-in',
     pathMatch: 'full'
   },
-
   {
-    path: 'manage',
-    children: [
-      {
-        path: 'auth',
-        children: [
-          {
-            path: 'sign-in',
-            redirectTo: '/manage/auth/sign-in',
-            pathMatch: 'full'
-          },
-          {
-            path: 'sign-out',
-            redirectTo: '/manage/auth/sign-out',
-            pathMatch: 'full'
-          },
-          {
-            path: 'forget-password',
-            redirectTo: '/manage/auth/forget-password',
-            pathMatch: 'full'
-          },
-          {
-            path: 'forget-password/account/:account/token/:token',
-            redirectTo: '/manage/auth/forget-password/account/:account/token/:token',
-            pathMatch: 'full'
-          }
-        ]
-      },
-      {
-        path: 'dashboard',
-        loadChildren: () => import('../modules/dashboard/dashboard.module').then(m => m.DashboardModule)
-      },
-    ]
+    path: 'auth/sign-out',
+    redirectTo: '/auth/sign-out',
+    pathMatch: 'full'
   },
-
+  {
+    path: 'auth/forget-password',
+    redirectTo: '/auth/forget-password',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('../modules/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
   {
     path: 'error',
     component: ErrorPage
